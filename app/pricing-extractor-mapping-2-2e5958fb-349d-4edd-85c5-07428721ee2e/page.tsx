6fdd3123e80f2b2f8ce43692948eb2174567b407
Rachel Hu <goldpiggy@berkeley.edu>
2024-09-30 16:56:16 -0700

    monthly subscription

--- app/pricing-extractor-mapping-2-2e5958fb-349d-4edd-85c5-07428721ee2e/page.tsx
+++ app/pricing-extractor-mapping-2-2e5958fb-349d-4edd-85c5-07428721ee2e/page.tsx
@@ -11,7 +11,7 @@ type Plan = {
   bgColor?: string;
   features: {
     autoCapture: boolean;
-    customSchemas: boolean;
+    autoRollover: boolean;
     clientOnboarding: boolean;
     customizationServices: boolean;
     customModelTraining: boolean;
@@ -29,7 +29,7 @@ const plans: Plan[] = [
     bgColor: 'bg-neutral-100',
     features: {
       autoCapture: true,
-      customSchemas: true,
+      autoRollover: true,
       clientOnboarding: false,
       customizationServices: false,
       customIntegrations: false,
@@ -44,7 +44,7 @@ const plans: Plan[] = [
     bgColor: 'bg-sky-200',
     features: {
       autoCapture: true,
-      customSchemas: true,
+      autoRollover: true,
       clientOnboarding: false,
       customizationServices: false,
       customIntegrations: false,
@@ -59,7 +59,7 @@ const plans: Plan[] = [
     bgColor: 'bg-[#bcc6cc]',
     features: {
       autoCapture: true,
-      customSchemas: true,
+      autoRollover: true,
       clientOnboarding: true,
       customizationServices: false,
       customIntegrations: false,
@@ -74,7 +74,7 @@ const plans: Plan[] = [
     bgColor: 'bg-teal-600 text-white',
     features: {
       autoCapture: true,
-      customSchemas: true,
+      autoRollover: true,
       clientOnboarding: true,
       customizationServices: true,
       customIntegrations: false,
@@ -89,7 +89,7 @@ const plans: Plan[] = [
     pages: 'contact-us',
     features: {
       autoCapture: true,
-      customSchemas: true,
+      autoRollover: true,
       clientOnboarding: true,
       customizationServices: true,
       customIntegrations: true,
@@ -201,11 +201,11 @@ const PricingPage = () => {
               ))}
             </tr>
             <tr>
-              <td className={featureRowHeaderStyle}>Mapping to your customized schemas</td>
+              <td className={featureRowHeaderStyle}>Credits automatically roll over</td>
               {plans.map((plan, index) => (
                 <td key={index} className={checkCellStyle}>
                   <div className={checkCellDivStyle}>
-                    <FeatureCheck enabled={plan.features.customSchemas} />
+                    <FeatureCheck enabled={plan.features.autoRollover} />
                   </div>
                 </td>
               ))}
@@ -268,7 +268,7 @@ const PricingPage = () => {
 
       <div className="h-[100px] p-8 rounded-xl w-screen-xl items-center justify-center">
         <ul>
-          <li>* No hidden fees; 30 days money back guaranteed.</li>
+          <li>* No hidden fees; Monthly pay as you go; 30 days money back guaranteed.</li>
         </ul>
         <ul>
           <li>
