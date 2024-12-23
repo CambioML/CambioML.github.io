f6895ff73ee52dbc85ce8566730f32d6148ed09f
remi-guan <remi.guan@outlook.com>
2024-12-23 17:33:38 +0800

    fix: Incorrect user-interaction of input box component

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -55,8 +55,8 @@ const Input = ({
           rounded-md
           outline-none
           transition
-          disabled: opacity-70
-          disabled: cursor-not-allowed
+          disabled:opacity-70
+          disabled:cursor-not-allowed
           ${formatPrice ? 'pl-9' : 'pl-4'}
           ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
           ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
@@ -72,11 +72,12 @@ const Input = ({
           top-5
           z-10
           origin-[0]
-          ${formatPrice ? 'left-9' : 'left-4'}
+          pointer-events-none
           peer-placeholder-shown:scale-100
           peer-placeholder-shown:translate-y-0
           peer-focus:scale-75
           peer-focus:-translate-y-4
+          ${formatPrice ? 'left-9' : 'left-4'}
           ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
         `}
       >
